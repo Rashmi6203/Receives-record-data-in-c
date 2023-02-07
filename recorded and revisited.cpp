b31fead31457b21079c1486523cfb44a7557f813
Rashmi kumari <102369387+Rashmi6203@users.noreply.github.com>
2023-02-07 12:29:27 +0530

    Add files via upload

---
+++ recorded and revisited.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	FILE *fp;
+	char another='y';
+	struct emp
+	{
+		char name[40];
+		int age;
+		float bs;
+	};
+	struct emp e;
+	
+	fp = fopen("EMP.DAT","wb");
+	if (fp==NULL)
+	{
+		puts ("Cannot open file");
+		exit(1);
+	}
+	while(another =='Y')
+	{
+		printf("\nEnter name , age and basic salary: ");
+		scanf("%s%d%f",e.name,&e.age,&e.bs);
+		fwrite(&e,sizeof(e), 1, fp);
+		
+		printf("Add another record(Y/N)");
+		fflush(stdin);
+		another=getche();
+		
+	}
+	fclose(fp);
+	return 0;
+}
